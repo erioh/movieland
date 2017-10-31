f271f904d20302ba060459bb6de50ec61e7d5901
sergeydemenkov <sergey.demenkov@gmail.com>
2017-10-31 22:23:47 +0200

    merjed

--- src/main/java/com/luxoft/sdemenkov/movielend/dao/jdbc/CountryDao.java
+++ src/main/java/com/luxoft/sdemenkov/movielend/dao/jdbc/CountryDao.java
@@ -5,9 +5,7 @@ import com.luxoft.sdemenkov.movielend.model.Movie;
 
 import java.util.List;
 
-/**
- * Created by dp-ptcstd-43 on 10/30/2017.
- */
+
 public interface CountryDao {
     List<Country> getCountryListByMovie(Movie movie);
 
--- src/main/java/com/luxoft/sdemenkov/movielend/dao/jdbc/MovieDao.java
+++ src/main/java/com/luxoft/sdemenkov/movielend/dao/jdbc/MovieDao.java
@@ -4,9 +4,7 @@ import com.luxoft.sdemenkov.movielend.model.Movie;
 
 import java.util.List;
 
-/**
- * Created by dp-ptcstd-43 on 10/30/2017.
- */
+
 public interface MovieDao {
     List<Movie> getAllMovies();
 
